3cb6f787f08fa2e6a54764bc7f1ec2a791e27c8e
igorcardosoy-zorin <igorfilipicardoso@gmail.com>
2024-12-27 12:39:22 -0300

    refactor: routing by creating a separate Router component for better organization

--- loja-do-groot-web/src/App.tsx
+++ loja-do-groot-web/src/App.tsx
@@ -1,14 +1,9 @@
 import { ClerkLoaded, SignedIn, SignedOut } from '@clerk/clerk-react'
-import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer'
 import Navbar from './components/Navbar'
-import AboutUs from './pages/AboutUs'
-import Home from './pages/Home'
+import Router from './components/Router'
 import LoginPage from './pages/LoginPage'
-import Product from './pages/Product'
-import Products from './pages/Products'
-import Register from './pages/Register'
 
 function App() {
   return (
@@ -21,14 +16,7 @@ function App() {
         </SignedIn>
         <main>
           <SignedIn>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/home' element={<Home />} />
-              <Route path='/register' element={<Register />} />
-              <Route path='/products' element={<Products />} />
-              <Route path='/product/:id' element={<Product />} />
-              <Route path='/about-us' element={<AboutUs />} />
-            </Routes>
+            <Router />
           </SignedIn>
           <SignedOut>
             <LoginPage />
---
+++ loja-do-groot-web/src/components/Router.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+import AboutUs from '../pages/AboutUs'
+import Home from '../pages/Home'
+import Product from '../pages/Product'
+import Products from '../pages/Products'
+import Register from '../pages/Register'
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/products' element={<Products />} />
+      <Route path='/product/:id' element={<Product />} />
+      <Route path='/about-us' element={<AboutUs />} />
+    </Routes>
+  )
+}
+export default Router
